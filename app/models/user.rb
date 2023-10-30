f50a09f2a4250de65059e60e693d129b23245df9
Chris Cullinane <chriscullinane@protonmail.com>
2023-10-30 13:53:39 -0800

    user setup

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :subscriptions
+
+  validates :first_name, :last_name, :email, :address, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email format" }
+  validates :email, uniqueness: true
+end
