7159fb2857b22a0b13185f32bb91a570f624a1d9
Chris Cullinane <chriscullinane@protonmail.com>
2023-10-31 13:08:34 -0800

    subscriptions require login

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,5 @@
 class SubscriptionsController < ApplicationController
+  before_action :require_logged_in
   before_action :set_subscription, only: [:update]
 
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :address)
+    params.permit(:first_name, :last_name, :email, :address, :password, :password_confirmation)
   end
 end
