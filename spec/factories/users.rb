47dae8d5d7ae123de262d8f4a9ad26e8e7913904
Chris Cullinane <chriscullinane@protonmail.com>
2023-10-31 12:49:23 -0800

    user factory update

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     address { Faker::Address.full_address }
+    password { 'test123456'}
+    password_confirmation { 'test123456'}
   end
 end
