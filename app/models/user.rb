b32ab32fb278d515ddc6e4d18e8ac9216a4d37d6
Chris Cullinane <chriscullinane@protonmail.com>
2023-10-31 12:49:10 -0800

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :subscriptions
 
   validates :first_name, :last_name, :email, :address, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:address) }
-
     it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:address) }
+    it { should have_secure_password }
   end
 
   describe 'associations' do
