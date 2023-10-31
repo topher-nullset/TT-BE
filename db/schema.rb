3875a54c63c905c436595bd4731234da13649b4b
Chris Cullinane <chriscullinane@protonmail.com>
2023-10-31 12:46:40 -0800

    setup db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_30_213808) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_194749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_30_213808) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "subscriptions", "users"
