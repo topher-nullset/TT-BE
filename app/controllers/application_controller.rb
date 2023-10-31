f6b1e0eeae925b1368540a2395a2479254006106
Chris Cullinane <chriscullinane@protonmail.com>
2023-10-31 13:06:58 -0800

    login methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!@current_user
+  end
+
+  def require_logged_in
+    unless logged_in?
+      render json: { error: 'You must be logged in to access this section' }, status: 401
+    end
+  end
 end
